b1aaeffecdf8e5560d4f72a9deee88d75e531ead
Imran <imran.yasin@softcircles.org>
2023-09-15 15:45:46 +0500

    fake api implimented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>PWA with react js </h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+          textAlign: "center",
+        }}
+      >
+        {data &&
+          data?.slice(0, 10).map((el) => (
+            <div key={el.id}>
+              <h1>{el.title.slice(0, 10)}</h1>
+              <picture>
+                <source media="(min-width:650px)" srcSet={el.url} />
+                <source media="(min-width:465px)" srcSet={el.url} />
+                <img src={el.url} alt="Flowers" style={{ width: "auto" }} />
+              </picture>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
